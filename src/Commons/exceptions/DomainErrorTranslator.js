cce4cf0523b123870dba82bf9c2bbdd1a154d91c
vysf <yusufumar@student.untan.ac.id>
2023-01-08 00:18:17 +0700

    detail comment entities added

--- src/Commons/exceptions/DomainErrorTranslator.js
+++ src/Commons/exceptions/DomainErrorTranslator.js
@@ -23,6 +23,8 @@ DomainErrorTranslator._directories = {
   'NEW_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat membuat comment baru karena tipe data tidak sesuai'),
   'DETAIL_THREAD.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('harus mengirimkan id, title, body, date, username, dan comments'),
   'DETAIL_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat menampilkan detail thread karena tipe data tidak sesuai'),
+  'DETAIL_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('harus mengirimkan id, username, date dan content'),
+  'DETAIL_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat menampilkan detail comment karena tipe data tidak sesuai'),
 };
 
 module.exports = DomainErrorTranslator;
