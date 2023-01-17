fd05f10fb07af0a9e5d2c6582f4951066a8fe566
vysf <yusufumar@student.untan.ac.id>
2023-01-17 16:23:57 +0700

    ♻️  organize create server

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -1,7 +1,9 @@
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+
 const ClientError = require('../../Commons/exceptions/ClientError');
 const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator');
+
 const users = require('../../Interfaces/http/api/users');
 const authentications = require('../../Interfaces/http/api/authentications');
 const threads = require('../../Interfaces/http/api/threads');
